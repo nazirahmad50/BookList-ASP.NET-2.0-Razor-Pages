6c02cff58879e39a547250e7d5d37d485a2895f4
Nazir <n_ahmad20@live.co.uk>
2019-05-26 12:54:22 +0100

    Add project files.

---
+++ BookList_Razor/Model/ApplicaitonDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_Razor.Model
+{
+    public class ApplicaitonDbContext: DbContext
+    {
+        public ApplicaitonDbContext (DbContextOptions<ApplicaitonDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+            
+      }
+}
---
+++ BookList_Razor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList_Razor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookList_Razor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private ApplicaitonDbContext _db;
+        [TempData]
+        public string Message { get; set; }
+
+        //using dependency injeciton
+        public CreateModel(ApplicaitonDbContext db)
+        {
+            _db = db;
+
+        }
+
+        //bind the property to the handler or method
+        //instead of using 'OnPost(Book book)'
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+
+            }
+            else
+            {
+                _db.Books.Add(Book);
+                await _db.SaveChangesAsync();
+
+                Message = "New Book Added";
+
+                return RedirectToPage("Index");
+
+            }
+        }
+    }
+}
